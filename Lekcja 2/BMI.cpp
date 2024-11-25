dc01e1d4c0463f3f47ca8029368cc22dda16b4c7
Toxy0 <omega2002x@gmail.com>
2024-11-25 15:03:16 +0100

    Lekcja 2

--- Lekcja 2/BMI.cpp
+++ Lekcja 2/BMI.cpp
@@ -6,9 +6,19 @@ int main(){
 	float m; 
 	cout <<"Podaj Wzrost (w metrach):"; cin >> m;
 	float bmi = kg / (m * m);
-	cout <<"Twoje BMI wynosi:" << bmi ; if (bmi <=18.5) {
-    cout << "Niedowaga" << endl;
+	cout <<"Twoje BMI wynosi:" << bmi << endl;
+	if (bmi <=18.5) cout << "Niedowaga" << endl;
+{
+
+	if ((bmi >= 18.5) && (bmi <= 24.9))
+	cout <<"W Normie" << endl;
 }
-if (bmi )
+{
+	if ((bmi >= 25) && (bmi <= 29.9))
+	cout <<"Nadwaga" << endl; 
+	}
+	{
+	if (bmi >=30) cout << "Otylosc" << endl;
+	}
 	return 0;
 }
---
+++ Lekcja 2/Sprawdzanie parzystości i podzielności przez 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int liczba;
+int b = 3;
+int c = 2;
+cout <<"Podaj Liczbe Calkowita:"; cin >> liczba;
+if (liczba/b)
+return 0;
+}
+
