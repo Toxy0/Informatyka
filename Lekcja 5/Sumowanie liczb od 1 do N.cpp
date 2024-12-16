13c662efd2d856182582f6583f78902b499fe543
Toxy0 <omega2002x@gmail.com>
2024-12-16 15:03:46 +0100

    Lekcja 5

---
+++ Lekcja 5/Sumowanie liczb od 1 do N.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+int N;
+int suma;
+cout <<("Podaj Liczbe N: "); cin >> N;
+for(int N = N; suma > N; N++)
+{
+	cout <<("Suma liczb od 1 do")<< N << ("wynosi:") << suma << 
+}
+	return  0;
+}
