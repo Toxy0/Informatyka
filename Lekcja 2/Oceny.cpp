0916660cda7886a5e7f29a60e503e46bce5ff26e
Toxy0 <omega2002x@gmail.com>
2025-03-08 12:11:37 +0100

    Lekcja 2

--- Lekcja 2/Oceny.cpp
+++ Lekcja 2/Oceny.cpp
@@ -1,9 +1,8 @@
-
 #include <iostream>
 using namespace std;
 int main(){
-	int liczba ;
-	cout << "Podaj wynik egzaminu (liczba punktow) : "; 
+int liczba ;
+cout << "Podaj wynik egzaminu (liczba punktow) : "; 
 cin >> liczba; 	
 if ((liczba >= 0) && (liczba <= 49)) {
     cout << "Ocena Niedostaczena" << endl;
