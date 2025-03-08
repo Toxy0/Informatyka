5dee1504c134ae2abe7ded8862cd53d904e25eb6
Toxy0 <omega2002x@gmail.com>
2025-03-08 13:20:50 +0100

    Lekcja 5

---
+++ Lekcja 5/Liczby Nieparzyste Od 1 Do N.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+int N;
+cout << "Podaj liczbe N: ";
+cin >> N;
+cout << "Liczby nieparzyste od 1 do " << N << ":\n";
+    for (int i = 1; i <= N; i += 2) {
+        cout << i << "\n";
+    }
+	return 0;
+}
---
+++ Lekcja 5/Liczby Nieparzyste W Macierzy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int wiersze, kolumny;
+    cout << "Podaj liczbe wierszy: ";
+    cin >> wiersze;
+    cout << "Podaj liczbe kolumn: ";
+    cin >> kolumny;
+    int liczba = 1;
+    for (int i = 0; i < wiersze; i++) {
+        for (int j = 0; j < kolumny; j++) {
+            cout << liczba << "\t";
+            liczba += 2;
+        }
+        cout << "\n";
+    }
+	return 0;}
